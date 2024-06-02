0f0a2c16fe806f22b08d5d348a99b3423371d04b
Rodrigo Carvajal Fernandez <rodrigocf_92@icloud.com>
2024-06-02 09:40:00 -0700

    Commit to reproduce error.

--- lib/partitions_bug-stack.ts
+++ lib/partitions_bug-stack.ts
@@ -1,16 +1,60 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {RemovalPolicy} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {Key} from "aws-cdk-lib/aws-kms";
+import {CfnDataCatalogEncryptionSettings} from "aws-cdk-lib/aws-glue";
+import {Database, DataFormat, S3Table, Schema} from "@aws-cdk/aws-glue-alpha";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PartitionsBugStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const catalogKey = new Key(this, 'CatalogKey', {
+            enableKeyRotation: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PartitionsBugQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        new CfnDataCatalogEncryptionSettings(this, 'CatalogEncryption', {
+            dataCatalogEncryptionSettings: {
+                encryptionAtRest: {
+                    catalogEncryptionMode: 'SSE-KMS',
+                    sseAwsKmsKeyId: catalogKey.keyId,
+                },
+            },
+            catalogId: this.account,
+        });
+
+        const database = new Database(this, 'Database', {
+            databaseName: 'testdb'
+        });
+
+        new S3Table(this, 'MyTable', {
+            database: database,
+            columns: [
+                {
+                    name: 'col1',
+                    type: Schema.STRING,
+                },
+            ],
+            partitionKeys: [
+                {
+                    name: 'year',
+                    type: Schema.SMALL_INT,
+                },
+            ],
+            dataFormat: DataFormat.JSON,
+            enablePartitionFiltering: true,
+            partitionIndexes: [
+                {
+                    indexName: 'test',
+                    keyNames: [
+                        'year',
+                    ],
+
+                },
+            ],
+        });
+    }
 }
