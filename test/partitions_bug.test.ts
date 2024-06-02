ba3519a6c698c4afe2c8a14b1981d184a5205a63
Rodrigo Carvajal Fernandez <rodrigocf_92@icloud.com>
2024-06-02 09:24:16 -0700

    Initial commit

---
+++ test/partitions_bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PartitionsBug from '../lib/partitions_bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/partitions_bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PartitionsBug.PartitionsBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
